efec83851ab99545e3ccaeee9e26924103ca17f5
S. M. Mehedi <104342711+MehediPrime@users.noreply.github.com>
2023-01-11 21:24:27 +0600

    Focus On

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "./HomePage/Banner";
 import FocusedOn from "./HomePage/FocusedOn";
 import FollowStep from "./HomePage/FollowStep";
@@ -6,51 +6,85 @@ import ExpertIn from "./HomePage/ExpertIn";
 import "./css/Homepage.css";
 
 const Homepage = () => {
+  const style = {
+    height: "100vh",
+    width: "90vw",
+  };
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 1400px)")
+  );
+
   useEffect(() => {
-    var windowWidth = window.innerWidth;
-
-    var horizontalLength =
-      document.querySelector(".elementWrapper").scrollWidth;
-
-    var distanceFromTop =
-      document.querySelector(".horizontalSection").offsetTop;
-
-    var scrollDistance = distanceFromTop + horizontalLength - windowWidth;
-
-    document.querySelector(".horizontalSection").style.height =
-      horizontalLength + "px";
-
-    window.onscroll = function () {
-      var scrollTop = window.pageYOffset;
-      console.log(scrollTop);
-
-      if (scrollTop >= distanceFromTop && scrollTop <= scrollDistance) {
-        document.querySelector(".elementWrapper").style.transform =
-          "translateX(-" + (scrollTop - distanceFromTop) + "px)";
-      }
-    };
+    const interval = setInterval(() => {
+      setMatches(window.matchMedia("(min-width: 1400px)"));
+    }, 1000);
+    return () => clearInterval(interval);
   });
 
-  return (
-    <div className="horizontalSection">
-      <div className="stickyWrapper">
-        <div className="elementWrapper">
-          <div className="element">
-            <Banner />
-          </div>
-          <div className="element">
-            <FocusedOn />
-          </div>
-          <div className="element">
-            <ExpertIn />
-          </div>
-          <div className="element">
-            <FollowStep />
+  useEffect(() => {
+    if (matches.matches) {
+      console.log("te");
+      var windowWidth = window.innerWidth;
+
+      var horizontalLength =
+        document.querySelector(".elementWrapper").scrollWidth;
+
+      var distanceFromTop =
+        document.querySelector(".horizontalSection").offsetTop;
+
+      var scrollDistance = distanceFromTop + horizontalLength - windowWidth;
+
+      document.querySelector(".horizontalSection").style.height =
+        horizontalLength + "px";
+
+      window.onscroll = function () {
+        var scrollTop = window.pageYOffset;
+
+        if (
+          scrollTop >= distanceFromTop &&
+          scrollTop <= scrollDistance &&
+          matches.matches
+        ) {
+          document.querySelector(".elementWrapper").style.transform =
+            "translateX(-" + (scrollTop - distanceFromTop) + "px)";
+        }
+      };
+    }
+  });
+
+  if (matches.matches) {
+    return (
+      <div className="horizontalSection">
+        <div className="stickyWrapper">
+          <div className="elementWrapper">
+            <div className="element">
+              <Banner />
+            </div>
+            <div className="element">
+              <FocusedOn />
+            </div>
+            <div className="element">
+              <ExpertIn />
+            </div>
+            <div className="element">
+              <FollowStep />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <div style={style}>
+          <Banner />
+        </div>
+        <FocusedOn />
+        <ExpertIn />
+        <FollowStep />
+      </div>
+    );
+  }
 };
 
 export default Homepage;
